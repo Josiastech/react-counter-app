323a6d178f90f111dc52e684763f1536822039e6
Josias <josiastech@gmail.com>
2020-06-28 17:56:24 -0600

    feat(core): Use hooks in counter app

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,21 +1,33 @@
 // rafcp comando para crear un functional component
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-const CounterApp = ({ value }) => {
+const CounterApp = ({ value = 10 }) => {
+
+  const [counter, setCounter] = useState(value); // []
+
 
   // handleAdd
-  const handleAdd = ( e ) => {
-    // console.log(e);
-    
-    return () => console.log('Hola Mundo')
+  const handleAdd = () => {
+    setCounter(counter + 1);
+    // setCounter( (c) => c + 1 );
+  }
+
+  const handleSubs = () => {
+    setCounter( counter - 1);
+  }
+
+  const handleReset = () => {
+    setCounter(value);
   }
 
   return (
     <>
-      <h1>Counter App</h1>
-      <h2>{ value }</h2>
-      <button onClick={  handleAdd() }>+1</button>
+      <h1>CounterApp</h1>
+      <h2> {counter} </h2>
+      <button onClick={handleSubs}>-1</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleAdd}>+1</button>
     </>
   )
 }

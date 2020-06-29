9718354f4391692df444f0d27d8deb0b240747ac
Josias <josiastech@gmail.com>
2020-06-28 22:55:28 -0600

    feat(test): Add enzyme and refactor test with enzyme snapshot

--- src/tests/FirstApp.test.js
+++ src/tests/FirstApp.test.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import { render } from "@testing-library/react";
-// import '@testing-library/jest-dom';
 import FirstApp from '../FirstApp';
+import { shallow } from 'enzyme';
 
 
 
 //ahora se van a llevar a cabo pruebas que son excluisvas de React
 describe('Pruebas en <FirstApp />', () => {
 
-  test('debe mostrar el msje "Hola Mundo" ', () => {
-    //se debe renderizar el componente para poder realizar la prueba
-
-    const saludo = "Hola Mundo";
-
-    const { getByText } = render(<FirstApp saludo={saludo} />);
-
-    //wrapper.getByText()
-
-    expect(getByText(saludo)).toBeInTheDocument();
-
+  test('should show <FirstApp /> correctly', () => {
+    const saludo = 'Hola Mundo';
+    const wrapper = shallow(<FirstApp saludo={saludo} />)
+    expect(wrapper).toMatchSnapshot();
   })
+
 
 });
